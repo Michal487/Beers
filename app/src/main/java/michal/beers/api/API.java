535e28c2a4a4c835373ef5507de06a56bfb1d483
Michal <miwojcik87@gmail.com>
2018-04-30 19:20:26 +0200

    Create API

---
+++ app/src/main/java/michal/beers/api/API.java
@@ -0,0 +1,17 @@
+package michal.beers.api;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import michal.beers.data.Beer;
+import retrofit2.http.GET;
+
+public interface API {
+
+    String BASE_URL = "https://api.punkapi.com/v2/";
+
+    @GET("beers")
+    Single<List<Beer>> getBeers();
+
+
+}
