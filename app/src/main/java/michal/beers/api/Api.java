53737e219dbcc485dfb30bc3bd320d7a4edf80f9
Michal <miwojcik87@gmail.com>
2018-05-03 20:28:03 +0200

    Change API name to Api

--- app/src/main/java/michal/beers/api/Api.java
+++ app/src/main/java/michal/beers/api/Api.java
@@ -6,7 +6,7 @@ import io.reactivex.Single;
 import michal.beers.data.Beer;
 import retrofit2.http.GET;
 
-public interface API {
+public interface Api {
 
     String BASE_URL = "https://api.punkapi.com/v2/";
 
