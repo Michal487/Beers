015bf13f6ec0418dac47f7a2538e8f9c1c89361a
Michal <miwojcik87@gmail.com>
2018-05-03 20:28:30 +0200

    Add Retrofit

--- app/src/main/java/michal/beers/activity/MainActivity.java
+++ app/src/main/java/michal/beers/activity/MainActivity.java
@@ -3,8 +3,14 @@ package michal.beers.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 import butterknife.ButterKnife;
 import michal.beers.R;
+import michal.beers.api.Api;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
@@ -17,6 +23,13 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+
+        
     }
 
 
